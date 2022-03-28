a5dfa8b963a435fa3d8210071573d00fa4489c29
Talha Y <tycode0@gmail.com>
2022-03-28 19:52:29 +0200

    modified according to feedback

--- Week1/exercise1.js
+++ Week1/exercise1.js
@@ -13,60 +13,65 @@ db.connect((err) => {
   console.log('MySql connected.');
 });
 
-// db.query("DROP DATABASE IF EXISTS meetup", (error, result) => {
-//   if (error) throw error;
-//   console.log("Database has been dropped!");
-// });
+db.query("DROP DATABASE IF EXISTS meetup", (error, result) => {
+  if (error) throw error;
+  console.log("Database has been dropped!");
+});
 
-// Create database
-// let sql = `CREATE DATABASE meetup`;
-// db.query(sql, (err, result) => {
-//   if (err) throw err;
-//   console.log(result);
-//   console.log('Database has been created!');
-// });
+let createDB = `CREATE DATABASE meetup`;
+db.query(createDB, (err, result) => {
+  if (err) throw err;
+  console.log(result);
+  console.log('Database has been created!');
+});
 
-// Create tables
-// let sql = `CREATE TABLE Invitee(invitee_no int AUTO_INCREMENT PRIMARY KEY, invitee_name VARCHAR(100), invited_by VARCHAR(100))`;
-// db.query(sql, (err, result) => {
-//   if (err) throw err;
-//   console.log(result);
-//   console.log('Table Invitee created!');
-// });
+let useDB = `USE meetup`;
+db.query(useDB, (err, result) => {
+  if (err) throw err;
+  console.log(result);
+  console.log('Database has been selected!');
+});
 
-// let sql = `CREATE TABLE Room(room_no int, room_name VARCHAR(100), floor_number int)`;
-// db.query(sql, (err, result) => {
-//   if (err) throw err;
-//   console.log(result);
-//   console.log('Table Room created!');
-// });
+let createTable1 = `CREATE TABLE Invitee(invitee_no int AUTO_INCREMENT PRIMARY KEY, invitee_name VARCHAR(100), invited_by VARCHAR(100))`;
+db.query(createTable1, (err, result) => {
+  if (err) throw err;
+  console.log(result);
+  console.log('Table Invitee created!');
+});
 
-// let sql = `CREATE TABLE Meeting(meeting_no int, meeting_title VARCHAR(100), starting_time datetime, ending_time datetime, room_no int)`;
-// db.query(sql, (err, result) => {
-//   if (err) throw err;
-//   console.log(result);
-//   console.log('Table Meeting created!');
-// });
+let createTable2 = `CREATE TABLE Room(room_no int, room_name VARCHAR(100), floor_number int)`;
+db.query(createTable2, (err, result) => {
+  if (err) throw err;
+  console.log(result);
+  console.log('Table Room created!');
+});
 
-// let sql = `INSERT INTO Invitee (invitee_name, invited_by) VALUES('Gandalf', 'Elrond'), ('Aragorn', 'Arwen'), ('Legolas', 'Elrond'), ('Gimli', 'Elrond'),('Frodo','Bilbo')`;
-// db.query(sql, (err, result) => {
-//   if (err) throw err;
-//   console.log(result);
-//   console.log('Values added!');
-// });
+let createTable3 = `CREATE TABLE Meeting(meeting_no int, meeting_title VARCHAR(100), starting_time datetime, ending_time datetime, room_no int)`;
+db.query(createTable3, (err, result) => {
+  if (err) throw err;
+  console.log(result);
+  console.log('Table Meeting created!');
+});
 
-// let sql = `INSERT INTO Room VALUES(101, 'Rivendell', 0), (102, 'Isengard', 0), (103, 'Minas Trith', 1), (104, 'Minas Morgul', 1),(105, 'Lothlorien', 2)`;
-// db.query(sql, (err, result) => {
-//   if (err) throw err;
-//   console.log(result);
-//   console.log('Values added!');
-// });
+let insertRowsTable1 = `INSERT INTO Invitee (invitee_name, invited_by) VALUES('Gandalf', 'Elrond'), ('Aragorn', 'Arwen'), ('Legolas', 'Elrond'), ('Gimli', 'Elrond'),('Frodo','Bilbo')`;
+db.query(insertRowsTable1, (err, result) => {
+  if (err) throw err;
+  console.log(result);
+  console.log('Values added!');
+});
 
-// let sql = `INSERT INTO Meeting VALUES (001, 'First Meeting', '1000-01-01 00:00:00', '1000-01-01 01:00:00', 101), (002, 'Second Meeting', '1000-01-01 05:00:00', '1000-01-01 06:00:00', 102), (003, 'Third Meeting', '1000-01-01 09:00:00', '1000-01-01 11:00:00', 103), (004, 'Fourth Meeting', '1000-01-01 13:00:00', '1000-01-01 15:00:00', 104),(005, 'Fifth Meeting', '1000-01-01 17:00:00', '1000-01-01 21:00:00', 105)`;
-// db.query(sql, (err, result) => {
-//   if (err) throw err;
-//   console.log(result);
-//   console.log('Values added!');
-// });
+let insertRowsTable2 = `INSERT INTO Room VALUES(101, 'Rivendell', 0), (102, 'Isengard', 0), (103, 'Minas Trith', 1), (104, 'Minas Morgul', 1),(105, 'Lothlorien', 2)`;
+db.query(insertRowsTable2, (err, result) => {
+  if (err) throw err;
+  console.log(result);
+  console.log('Values added!');
+});
+
+let insertRowsTable3 = `INSERT INTO Meeting VALUES (001, 'First Meeting', '1000-01-01 00:00:00', '1000-01-01 01:00:00', 101), (002, 'Second Meeting', '1000-01-01 05:00:00', '1000-01-01 06:00:00', 102), (003, 'Third Meeting', '1000-01-01 09:00:00', '1000-01-01 11:00:00', 103), (004, 'Fourth Meeting', '1000-01-01 13:00:00', '1000-01-01 15:00:00', 104),(005, 'Fifth Meeting', '1000-01-01 17:00:00', '1000-01-01 21:00:00', 105)`;
+db.query(insertRowsTable3, (err, result) => {
+  if (err) throw err;
+  console.log(result);
+  console.log('Values added!');
+});
 
 db.end();
--- Week1/exercise2.js
+++ Week1/exercise2.js
@@ -13,64 +13,64 @@ db.connect((err) => {
   console.log('MySql connected.');
 });
 
-// let sql = `SELECT name FROM country WHERE population > 8000000`
-// db.query(sql, (error, result) => {
-//   if (error) throw error;
-//   console.log(result);
-// });
+let query1 = `SELECT name FROM country WHERE population > 8000000`
+db.query(query1, (error, result) => {
+  if (error) throw error;
+  console.log(result);
+});
 
-// let sql = `SELECT name FROM country WHERE name LIKE '%land%'`
-// db.query(sql, (error, result) => {
-//   if (error) throw error;
-//   console.log(result);
-// });
+let query2 = `SELECT name FROM country WHERE name LIKE '%land%'`
+db.query(query2, (error, result) => {
+  if (error) throw error;
+  console.log(result);
+});
 
-// let sql = `SELECT name FROM city WHERE population BETWEEN 500000 AND 999999`
-// db.query(sql, (error, result) => {
-//   if (error) throw error;
-//   console.log(result);
-// });
+let query3 = `SELECT name FROM city WHERE population BETWEEN 500000 AND 999999`
+db.query(query3, (error, result) => {
+  if (error) throw error;
+  console.log(result);
+});
 
-// let sql = `SELECT name FROM country WHERE continent = 'Europe'`;
-// db.query(sql, (error, result) => {
-//   if (error) throw error;
-//   console.log(result);
-// });
+let query4 = `SELECT name FROM country WHERE continent = 'Europe'`;
+db.query(query4, (error, result) => {
+  if (error) throw error;
+  console.log(result);
+});
 
-// let sql = `SELECT name FROM country ORDER BY surfaceArea DESC`;
-// db.query(sql, (error, result) => {
-//   if (error) throw error;
-//   console.log(result);
-// });
+let query5 = `SELECT name FROM country ORDER BY surfaceArea DESC`;
+db.query(query5, (error, result) => {
+  if (error) throw error;
+  console.log(result);
+});
 
-// let sql = `SELECT name FROM city WHERE countryCode = 'NLD'`;
-// db.query(sql, (error, result) => {
-//   if (error) throw error;
-//   console.log(result);
-// });
+let query6 = `SELECT name FROM city WHERE countryCode = 'NLD'`;
+db.query(query6, (error, result) => {
+  if (error) throw error;
+  console.log(result);
+});
 
-// let sql = `SELECT population FROM city WHERE name = 'Rotterdam'`;
-// db.query(sql, (error, result) => {
-//   if (error) throw error;
-//   console.log(result);
-// });
+let query7 = `SELECT population FROM city WHERE name = 'Rotterdam'`;
+db.query(query7, (error, result) => {
+  if (error) throw error;
+  console.log(result);
+});
 
-// let sql = `SELECT name FROM country ORDER BY surfaceArea DESC LIMIT 10`;
-// db.query(sql, (error, result) => {
-//   if (error) throw error;
-//   console.log(result);
-// });
+let query8 = `SELECT name FROM country ORDER BY surfaceArea DESC LIMIT 10`;
+db.query(query8, (error, result) => {
+  if (error) throw error;
+  console.log(result);
+});
 
-// let sql = `SELECT name FROM city ORDER BY population DESC LIMIT 10`;
-// db.query(sql, (error, result) => {
-//   if (error) throw error;
-//   console.log(result);
-// });
+let query9 = `SELECT name FROM city ORDER BY population DESC LIMIT 10`;
+db.query(query9, (error, result) => {
+  if (error) throw error;
+  console.log(result);
+});
 
-// let sql = `SELECT Sum(population) AS 'World Population' FROM country `;
-// db.query(sql, (error, result) => {
-//   if (error) throw error;
-//   console.log(result);
-// });
+let query10 = `SELECT Sum(population) AS 'World Population' FROM country `;
+db.query(query10, (error, result) => {
+  if (error) throw error;
+  console.log(result);
+});
 
 db.end();
